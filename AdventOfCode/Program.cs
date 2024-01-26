4ecb64aa7ece17a777fc48fa390e683e5544afae
frasm <maciej.fras@comarch.com>
2024-01-26 18:36:58 +0100

    day 5.5

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode.Solutions.Solutions2k23.Day5;
 
 var solution = new Day5();
-solution.SolveTaskOne();
+solution.SolveTaskTwo();
--- AdventOfCode/Solutions/Solutions2k23/Day5/Day5.cs
+++ AdventOfCode/Solutions/Solutions2k23/Day5/Day5.cs
@@ -37,9 +37,7 @@ public class Day5 : Utilities
             {
                 foreach (var y in x)
                 {
-                    Int64 target = y[0];
-                    Int64 source = y[1];
-                    Int64 range = y[2];
+                    var (target, source, range) = (y[0], y[1], y[2]);
                     if (value >= source && value <= source + range)
                     {
                         Int64 diff = source - target;
@@ -56,6 +54,56 @@ public class Day5 : Utilities
     
     public void SolveTaskTwo()
     {
+        _input = GetInputAsStringList("input_5_2k23.txt");
+        var headers = new string[]
+        {
+            "seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:", "water-to-light map:",
+            "light-to-temperature map:", "temperature-to-humidity map:", "humidity-to-location map:"
+        };
+        
+        var seeds = _input[0].Split(" ").Skip(1).Select(seed => Int64.Parse(seed)).ToList();
+        var maps = new List<List<List<Int64>>>();
+        
+        foreach (var header in headers)
+        {
+            var mapList =_input
+                .Skip(2)
+                .SkipWhile(line => line != header)
+                .Skip(1)
+                .TakeWhile(line => line != "")
+                .Select(line => line.Split(" ").Select(Int64.Parse).ToList())
+                .ToList();
+            maps.Add(mapList);
+        }
+
+        Int64 lowestValue = Int64.MaxValue;
+        
+        for(int i = 0; i<seeds.Count; i+=2)
+        {
+            Console.WriteLine(i);
+            Int64 start = seeds[i];
+            Int64 seedRange = start + seeds[i + 1] - 1;
+            for (long j = start; j<=seedRange; j++)
+            {
+                Int64 currentValue = j;
+                foreach (var x in maps)
+                {
+                    foreach (var y in x)
+                    {
+                        var (target, source, range) = (y[0], y[1], y[2]);
+                        if (currentValue >= source && currentValue <= source + range)
+                        {
+                            Int64 diff = currentValue - source;
+                            currentValue = target + diff;
+                            break;
+                        }
+                    }
+                }
+                lowestValue = long.Min(lowestValue, currentValue);
+            }
+        }
+
+        Console.WriteLine(lowestValue);
     }
     
 }
