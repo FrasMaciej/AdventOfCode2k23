5ca4263f0a307857111d2e6a159357308a19e83c
FrasMaciej <maciejfrs@gmail.com>
2024-02-20 01:48:38 +0100

    Prettier Day9

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.Solutions.Solutions2k23.Day9;
 
 var solution = new Day9();
+solution.SolveTaskOne();
 solution.SolveTaskTwo();
--- AdventOfCode/Solutions/Solutions2k23/Day9/Day9.cs
+++ AdventOfCode/Solutions/Solutions2k23/Day9/Day9.cs
@@ -2,34 +2,29 @@ namespace AdventOfCode.Solutions.Solutions2k23.Day9;
 
 public class Day9 : Utilities
 {
-
     public void SolveTaskOne()
     {
+        SolvePuzzle(false);
+    }
+    
+    public void SolveTaskTwo() {
+        SolvePuzzle(true);
+    }
+    
+    private void SolvePuzzle(bool reversed)
+    {
         var parsedInput = GetInputAsStringList("input_9_2k23.txt")
-            .Select(line => line.Split(" ").Select(Int32.Parse).ToList()).ToList();
+            .Select(line => line.Split(" ").Select(int.Parse).ToList());
         var sumOfExtrapolatedValues = parsedInput.Aggregate(0,
-            (acc, sequence) => acc + FindNextNumberInSequence(sequence));
+            (acc, sequence) => acc + FindNextNumberInSequence(reversed ? sequence.AsEnumerable().Reverse().ToList() : sequence));
         Console.WriteLine(sumOfExtrapolatedValues);
     }
-
-    public int FindNextNumberInSequence(List<int> sequence)
+    
+    private int FindNextNumberInSequence(List<int> sequence)
     {
         if (sequence.All(o => o == 0)) return 0;
         var newSequence = sequence.Zip(sequence.Skip(1), (current, next) => next - current).ToList();
         return sequence.Last() + FindNextNumberInSequence(newSequence);
-    }
-    
-    public void SolveTaskTwo()
-    {
-        var parsedInput = GetInputAsStringList("input_9_2k23.txt")
-            .Select(line => line.Split(" ").Select(Int32.Parse).ToList()).ToList();
-        var sumOfExtrapolatedValues = parsedInput.Aggregate(0,
-            (acc, sequence) =>
-            {
-                sequence.Reverse();
-                return acc + FindNextNumberInSequence(sequence);
-            });
-        Console.WriteLine(sumOfExtrapolatedValues);
     }
     
 }
