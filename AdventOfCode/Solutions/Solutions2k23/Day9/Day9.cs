4afd11b9277f712433915a69ac57fe84c1798d8c
FrasMaciej <maciejfrs@gmail.com>
2024-02-20 02:05:45 +0100

    Slightly shorter day9 :)

--- AdventOfCode/Solutions/Solutions2k23/Day9/Day9.cs
+++ AdventOfCode/Solutions/Solutions2k23/Day9/Day9.cs
@@ -2,16 +2,10 @@ namespace AdventOfCode.Solutions.Solutions2k23.Day9;
 
 public class Day9 : Utilities
 {
-    public void SolveTaskOne()
-    {
-        SolvePuzzle(false);
-    }
+    public void SolveTaskOne() => SolvePuzzle(false);
+    public void SolveTaskTwo() => SolvePuzzle(true);
     
-    public void SolveTaskTwo() {
-        SolvePuzzle(true);
-    }
-    
-    private void SolvePuzzle(bool reversed)
+    void SolvePuzzle(bool reversed)
     {
         var parsedInput = GetInputAsStringList("input_9_2k23.txt")
             .Select(line => line.Split(" ").Select(int.Parse).ToList());
@@ -20,11 +14,10 @@ public class Day9 : Utilities
         Console.WriteLine(sumOfExtrapolatedValues);
     }
     
-    private int FindNextNumberInSequence(List<int> sequence)
+    int FindNextNumberInSequence(List<int> sequence)
     {
         if (sequence.All(o => o == 0)) return 0;
         var newSequence = sequence.Zip(sequence.Skip(1), (current, next) => next - current).ToList();
         return sequence.Last() + FindNextNumberInSequence(newSequence);
     }
-    
 }
